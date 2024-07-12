ce8be968d2b4754c33f71f5b8270c0c1236e9125
Sondre Ulvik <sondreu.kodehode@gmail.com>
2024-07-12 12:06:26 +0200

    weather app update

--- Været/script.js
+++ Været/script.js
@@ -1,3 +1,4 @@
+
 const sitename = "https://github.com/Soul2606 sondreu.kodehode@gmail.com"
 const date_now = new Date()
 const current_time = {
@@ -8,7 +9,10 @@ const current_time = {
     minutes:date_now.getMinutes(),
     seconds:date_now.getSeconds()
 }
-
+const weather_data = JSON.parse(localStorage.getItem("weather data"))
+const timeseries_div = document.getElementById("timeseries")
+const year_p = document.getElementById("year")
+const month_p = document.getElementById("month")
 
 
 
@@ -39,4 +43,32 @@ document.getElementById("get-data-button").addEventListener("click", ()=>{
 
 
 
-document.getElementById("weather-data-string").textContent = localStorage.getItem("weather data")
+console.log(weather_data)
+
+const weather_data_timeseries = weather_data.properties.timeseries
+year_p.textContent = weather_data_timeseries[0].time.slice(0,4)
+month_p.textContent = weather_data_timeseries[0].time.slice(5,7)
+
+for (let i = 0; i < weather_data_timeseries.length; i++) {
+    const data = weather_data_timeseries[i].data
+    const time = weather_data_timeseries[i].time.slice(0, 13).split("-")
+    const data_instant = data.instant.details
+    const data_next_6_hours = data.next_6_hours.summary.symbol_code
+    const data_next_12_hours = data.next_12_hours.summary.symbol_code
+
+    console.log(time, data)
+
+    const timeseries_item_div = document.createElement("div")
+    timeseries_item_div.className = "timeseries-item"
+
+    const time_p = document.createElement("p")
+    time_p.textContent = time[2].slice(0,2) + ":" + time[2].slice(3,5)
+    time_p.style = "margin-right: 20px;"
+    timeseries_item_div.appendChild(time_p)
+
+    const air_pressure_at_sea_level_p = document.createElement("p")
+    air_pressure_at_sea_level_p.textContent = "Air pressure at sea level: " + data_instant.air_pressure_at_sea_level
+    timeseries_item_div.appendChild(air_pressure_at_sea_level_p)
+
+    timeseries_div.appendChild(timeseries_item_div)
+}
